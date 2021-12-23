5c502742f7d4c93df8063442c2af8f0f05b28214
Tâm Phan Thái <19520917@gm.uit.edu.vn>
2021-12-23 09:19:08 +0700

    Create project

---
+++ WholeSaleManagementApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WholeSaleManagementApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
